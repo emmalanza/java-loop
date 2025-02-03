0222428bda7eba88dc0dfdcbf7fb4d7b0ac947ef
emmalanza <emmalanza@hotmail.com>
2025-02-03 17:33:59 +0100

    create timetable and timetabletest class

---
+++ src/main/java/com/emma/TimeTable.java
@@ -0,0 +1,12 @@
+package com.emma;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TimeTable {
+
+    public List<String> getTimetable(int n) {
+        return new ArrayList<>();
+    }
+    
+}
---
+++ src/test/java/com/emma/TimeTableTest.java
@@ -0,0 +1,5 @@
+package com.emma;
+
+public class TimeTableTest {
+    
+}
